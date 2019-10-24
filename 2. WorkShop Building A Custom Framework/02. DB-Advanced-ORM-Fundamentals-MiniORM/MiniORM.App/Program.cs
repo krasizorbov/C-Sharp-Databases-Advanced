85a3e89e7df58ba036a6cb52017bd8b448790ec2
Krasimir Zorbov <49094272+krasizorbov@users.noreply.github.com>
2019-10-24 12:34:57 +0300

    Add files via upload

---
+++ 2. WorkShop Building A Custom Framework/02. DB-Advanced-ORM-Fundamentals-MiniORM/MiniORM.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server = DESKTOP-H5R51FC\SQLEXPRESS01; Integrated Security = true; Initial Catalog = MiniORM";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+            var employee = context.Employees.Last();
+            employee.FirstName = "KRASIMIR";
+            context.SaveChanges();
+
+        }
+    }
+}
