37c3cd0862ed579fa76e736a8222980606827153
Krasimir Zorbov <49094272+krasizorbov@users.noreply.github.com>
2019-11-04 08:44:57 +0200

    Add files via upload

---
+++ 5. Entity Relations/2. StudentSystem Seed/P01_StudentSystem/Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+    internal class Configuration
+    {
+        public const string ConnectionString = @"Server = DESKTOP-O2CP5S2\SQLEXPRESS;Database = StudentSystem; Integrated Security = True;";
+    }
+}
