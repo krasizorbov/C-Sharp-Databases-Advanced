7baeb41d6aba194056f050d8b54a6a90d8277ffa
Krasimir Zorbov <49094272+krasizorbov@users.noreply.github.com>
2019-11-07 19:59:20 +0200

    Delete Configuration.cs

--- 6. Advanced Queries/BookShop/BookShop.Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BookShop.Data
-{
-    internal class Configuration
-    {
-        internal static string ConnectionString => @"Server= whatever;Database=BookShop;Integrated Security=True;";
-    }
-}
