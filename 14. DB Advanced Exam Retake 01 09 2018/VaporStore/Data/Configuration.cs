37c696a263275c1ac8df3cbafb90bfbe214e58e8
Krasimir Zorbov <49094272+krasizorbov@users.noreply.github.com>
2019-12-04 05:46:45 +0200

    Update Configuration.cs

--- 14. DB Advanced Exam Retake 01 09 2018/VaporStore/Data/Configuration.cs
+++ 14. DB Advanced Exam Retake 01 09 2018/VaporStore/Data/Configuration.cs
@@ -3,6 +3,6 @@
 	public static class Configuration
 	{
 		public static string ConnectionString =
-            @"Server=DESKTOP-H5R51FC\SQLEXPRESS01;Database=VaporStore;Trusted_Connection=True";
+            @"Server=.;Database=VaporStore;Trusted_Connection=True";
 	}
 }
